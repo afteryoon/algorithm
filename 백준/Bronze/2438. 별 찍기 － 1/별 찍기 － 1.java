92193e050bf50c561c81c9d47e079290785d95e9
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-03-27 10:28:59 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 284 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,18 +1,16 @@
-
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
-
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int n= Integer.valueOf(br.readLine());
 		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j <= i; j++) {
+		for(int row=1; row<=n; row++) {
+			for(int col=0; col<row; col++ ) {
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println("");
 		}
-			
-	}
+    }
 }
