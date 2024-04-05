5abb701bc8368abba10a4f396ed7b7c2dd913169
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-04-05 13:10:10 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,8 +8,13 @@ import java.util.StringTokenizer;
 public class Main {
 	/*
 	 * ATM 
-	 * 메모리
-	 * 시간
+	 * 
+	 * 메모리 
+	 * 시간 
+	 * 
+	 * O(NlogN)일 때
+	 * 메모리 14384
+	 * 시간 132
 	 * 
 	 * 시간복잡도
 	 * 
@@ -18,29 +22,39 @@ public class Main {
 	 * 처음 주어지는 사람의 수 k
 	 * k를 내림차순으로 정렬
 	 * 각 시간을 더한 값을 총 시간값에 더해줌
+	 * +수정 후
+	 * 최대값을 기준으로 분수 별로 배열에 저장
+	 * 분수 별로 더하여 최대 값을 구함
 	 * 
 	 */
 	public static void main(String[] args)throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-	
 		int k= Integer.parseInt(br.readLine());
-		int[] arr = new int[k];
-
+		//최대시간인 1001분을 기준으로 잡음
+		
 		StringTokenizer st= new StringTokenizer(br.readLine()," ");
+		int[] arr = new int[1001];
+		//몇분이 몇개인지 저장
 		for (int i = 0; i <k; i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+			arr[Integer.parseInt(st.nextToken())]++;
 		}
 		
-		Arrays.sort(arr);
+		int prev = 0;	// 대기시간 누적합
+		int sum = 0;	// 대기시간 총합
 		
-		int minWaiting=0;
-		int add=0;
-		for (int i = 0; i < arr.length; i++) {
-			add+=arr[i];
-			minWaiting+=add;
+		
+		for (int i = 1; i <1001 ; i++) {
+			
+			while (arr[i]-- > 0) {
+				// 이전까지의 대기시간과 현재 사람이 걸리는 시간을 더해준다.
+				sum += (i + prev);
+				// 이전까지의 누적합에 현재 걸리는 시간을 더해준다.
+				prev += i;
+			}
 		}
 		
-		System.out.println(minWaiting);
+		System.out.println(sum);
 			
 	}
+
 }
