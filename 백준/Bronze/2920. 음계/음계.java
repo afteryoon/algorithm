a8624a3f234286213ccad73f18183d91a84a2199
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-04-14 23:44:36 +0900

    [Bronze II] Title: 음계, Time: 124 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,21 +1,20 @@
 
-
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String str= br.readLine();
+        String answer="mixed";
+        if(str.equals("1 2 3 4 5 6 7 8"))
+            answer="ascending";
+        else if (str.equals("8 7 6 5 4 3 2 1")) {
+            answer="descending";
+        }
 
-		Scanner sc = new Scanner(System.in);
-		
-		String arr=sc.nextLine().trim();
-		String result="mixed";
-		
-		if(arr.equals("1 2 3 4 5 6 7 8"))
-			result="ascending";
-		else if(arr.equals("8 7 6 5 4 3 2 1"))
-			result="descending";
-	
-		
-		System.out.println(result);
-	}
+        System.out.println(answer);
+    }
 }
