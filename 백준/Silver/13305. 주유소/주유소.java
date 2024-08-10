18fc40ef51758392e9e084907ba1ac43573fc396
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-08-10 17:43:33 +0900

    [Silver III] Title: 주유소, Time: 440 ms, Memory: 36184 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long[] distances = new long[n-1];
+		long[] prices = new long[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n-1; i++) {
+			distances[i] = Long.parseLong(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			prices[i] = Long.parseLong(st.nextToken());
+		}
+
+		long minPrice = prices[0];
+		long totalPrice = 0;
+
+		for (int i = 0; i < distances.length; i++) {
+			if(minPrice>prices[i]) minPrice=prices[i];
+			totalPrice += minPrice*distances[i];
+		}
+
+		System.out.println(totalPrice);
+	}
+
+}
