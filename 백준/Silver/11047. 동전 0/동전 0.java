c3f7cd1d8f1ba08d60a038975d0fc1b80059f52a
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-08-26 14:43:03 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        int[] coins = new int[N];
-        for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
-        
-        int count = 0;
-        for (int i = N - 1; i >= 0; i--) {
-            if (K >= coins[i]) {
-                count += K / coins[i];
-                K %= coins[i];
-            }
-        }
-        
-        System.out.println(count);
-    }
+	//11047
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int count = 0;
+		for (int i = arr.length-1; i >= 0; i--) {
+			if (k >= arr[i]) {
+				count += k / arr[i];
+				k %= arr[i];
+			}
+			if (k == 0) break; 
+		}
+
+		System.out.println(count);
+	}
 }
