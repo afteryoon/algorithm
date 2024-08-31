5e203e8c77786a3c356049aaacda5462d6109847
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-08-31 17:55:07 +0900

    [Gold V] Title: 적록색약, Time: 124 ms, Memory: 16792 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,89 +1,63 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	//10026
-	//
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static char[][] graph;
+    static boolean[][] visited;
+    static int n;
 
-	static int[] dx = {-1,0,0,1};
-	static int[] dy = {0,-1,1,0};
-	static char[][] graph;
-	static boolean[][] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        graph = new char[n][n];
 
-	static int n;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		graph = new char[n][n];
-		visit = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = br.readLine().toCharArray();
+        }
 
-		for (int i = 0; i < n; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < n; j++) {
-				graph[i][j] = line.charAt(j);
-			}
-		}
-		StringBuilder sb = new StringBuilder();
+        int normalCount = countRegions(false);
+        int colorBlindCount = countRegions(true);
 
-		int count1 = 0;
-		int count2 = 0;
+        System.out.println(normalCount + " " + colorBlindCount);
+    }
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(!visit[i][j]){
-					count1++;
-					dfs(i,j);
-				}
-			}
-		}
-		visit = new boolean[n][n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(!visit[i][j]){
-					count2++;
-					dfs2(i,j);
-				}
-			}
-		}
+    private static int countRegions(boolean isColorBlind) {
+        visited = new boolean[n][n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j, graph[i][j], isColorBlind);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
-		System.out.println(count1 + " " + count2);
-	}
+    private static void dfs(int i, int j, char color, boolean isColorBlind) {
+        visited[i][j] = true;
 
-	private static void dfs(int i,int j){
-		visit[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            int ny = i + dy[k];
+            int nx = j + dx[k];
 
-		for (int k = 0; k < 4; k++) {
-			int ny = i+dy[k];
-			int nx = j+dx[k];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < n && !visited[ny][nx]) {
+                if (isSameColor(color, graph[ny][nx], isColorBlind)) {
+                    dfs(ny, nx, graph[ny][nx], isColorBlind);
+                }
+            }
+        }
+    }
 
-			if(nx >=0 && ny >=0 && nx<n  && ny< n
-			&& !visit[ny][nx] && graph[i][j] == graph[ny][nx]){
-				dfs(ny,nx);
-			}
-		}
-	}
-
-	private static void dfs2(int i,int j){
-		visit[i][j] = true;
-
-
-		for (int k = 0; k < 4; k++) {
-			int ny = i+dy[k];
-			int nx = j+dx[k];
-
-			if(nx >=0 && ny >=0 && nx<n  && ny< n
-				&& !visit[ny][nx]){
-				if(graph[i][j] =='G' || graph[i][j] =='R' ) {
-					if(graph[ny][nx] == 'G' ||graph[ny][nx] == 'R') {
-						dfs2(ny, nx);
-					}
-				}else if(graph[i][j] == 'B' &&  graph[ny][nx] =='B'){
-					dfs2(ny, nx);
-				}
-			}
-		}
-	}
-
+    private static boolean isSameColor(char c1, char c2, boolean isColorBlind) {
+        if (isColorBlind) {
+            if (c1 == 'B') return c2 == 'B';
+            return c2 != 'B';
+        }
+        return c1 == c2;
+    }
 }
