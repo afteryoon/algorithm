5fa923dba213508176cc9027e266fabbc097e57c
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-10-08 12:58:41 +0900

    [level 0] Title: 이어 붙인 수, Time: 4.21 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,20 +1,6 @@
-import java.util.stream.*;
-
 class Solution {
     public int solution(int[] num_list) {
-        String evenNumbers = IntStream.range(0, num_list.length)
-            .filter(i -> num_list[i] % 2 == 0)
-            .mapToObj(i -> String.valueOf(num_list[i]))
-            .collect(Collectors.joining());
-
-        String oddNumbers = IntStream.range(0, num_list.length)
-            .filter(i -> num_list[i] % 2 != 0)
-            .mapToObj(i -> String.valueOf(num_list[i]))
-            .collect(Collectors.joining());
-
-        int evenSum = evenNumbers.isEmpty() ? 0 : Integer.parseInt(evenNumbers);
-        int oddSum = oddNumbers.isEmpty() ? 0 : Integer.parseInt(oddNumbers);
-
-        return evenSum + oddSum;
+        int answer = 0;
+        return answer;
     }
 }
