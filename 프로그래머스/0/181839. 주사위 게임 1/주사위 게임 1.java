4f0b74d8dbe1fe76298b5b354d517f8888287a9d
afteryoon <126151164+afteryoon@users.noreply.github.com>
2024-10-14 15:30:57 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        boolean fa = a % 2 ==0;
+        boolean fb = b % 2 ==0;
+        if(!fa && !fb)
+            answer=a*a+b*b;
+        else if(fa && fb)
+            answer = a-b <0 ? (a-b)*-1 : a-b ;
+        else
+            answer = 2*(a+b);
+        return answer;
+    }
+}
