32703d396e78eb2fa9481af830c59734d9c6d55c
afteryoon <126151164+afteryoon@users.noreply.github.com>
2025-01-29 05:06:57 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18144 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        System.out.println(dp(n));
-    }
-    private static int dp(int n){
-        int[] dp=new int[n+1];
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0)
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0)
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        }
-        return dp[n];
-    }
+		System.out.print(dp(n));
+
+	}
+
+	public static int dp(int n){
+		if( n == 1)
+			return 0;
+
+		int[] dp = new int[n + 1];
+
+		for (int i=2; i<=n; i++) {
+			dp[i] = dp[i-1] + 1;
+
+			if(i%2==0)
+				dp[i] = Math.min(dp[i],dp[i/2]+1);
+			if(i%3==0)
+				dp[i] = Math.min(dp[i],dp[i/3]+1);
+		}
+
+		return dp[n];
+	}
 }
