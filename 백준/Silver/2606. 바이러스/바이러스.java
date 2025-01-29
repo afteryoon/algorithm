c2e3c627a773d7676b2f2fc95640b8e84f014840
afteryoon <126151164+afteryoon@users.noreply.github.com>
2025-01-29 19:14:25 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,59 +1,40 @@
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-/*
- * 백준 2606 바이러스 dfs
- * 메모리	
- * 시간		
- * 
- */
-	static ArrayList<Integer>[] al;
-	static boolean[] visited;
-	
-	public static void main(String[] args) throws  IOException {
+	static List<List<Integer>> graph;
+	static boolean[] isVisit;
+	public static void main(String[] args ) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n= Integer.parseInt(br.readLine());
-		int m= Integer.parseInt(br.readLine());
-		al= new ArrayList[n+1];
-		for (int i = 1; i <=n; i++) {
-			al[i]=new ArrayList<>();
-		}
-		
-		for (int i = 0; i <m; i++) {
-			StringTokenizer st= new StringTokenizer(br.readLine());
-			int node=Integer.parseInt(st.nextToken());
-			int edge=Integer.parseInt(st.nextToken());
-			al[node].add(edge);
-			al[edge].add(node);
-		}
-		
-		visited= new boolean[n+1];
-		int count=dfs(1);
-		
-		StringBuilder sb= new StringBuilder();
-		//1번 빼기
-		sb.append(count-1);
-		System.out.println(sb);
-	}
-	
-	private static int dfs(int root) {
-		int count=1;
-		visited[root]=true;
+		int node = Integer.parseInt(br.readLine());
+		int edge = Integer.parseInt(br.readLine());
+		isVisit = new boolean[node+1];
+		graph = new ArrayList<>();
 
-		for(int next : al[root]) {
-			//방문하지 않았으면 탐색
-			if(!visited[next]) 
-				count+=dfs(next);
+		for ( int i=0; i<=node; i++) {
+			graph.add(new ArrayList<>());
 		}
-		
+
+		for (int i = 0; i<edge; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+		}
+		System.out.println(dfs(1)-1);
+	}
+
+	private static int dfs(int node) {
+		isVisit[node] = true;
+		int count =1;
+		for (int next : graph.get(node)) {
+			if(!isVisit[next]) {
+				count += dfs(next);
+			}
+		}
 		return count;
 	}
-
 }
