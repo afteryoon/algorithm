2318a81baf35104cb5018323df05367fe3495276
afteryoon <126151164+afteryoon@users.noreply.github.com>
2025-02-06 00:30:32 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 168 ms, Memory: 18500 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -1,51 +1,34 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-/*
- * 백준 1863 스카이라인 쉬운거 G4
- * 메모리
- * 시간
- * 시간복잡도
- * 
- * 문제
- * 높이가 낮아질 때 마다 건물이 추가되는 것을 알 수 있음, -> 건물이 낮아지면 pop
- * 같은 높이일 땐 stack에 추가할 필요 없음
- */
 public class Main {
 
-	public static void main(String[] args) throws  IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(br.readLine());
-		//건물 수를 셀 count
-		int count=0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st= new StringTokenizer(br.readLine()," ");
-			int x=Integer.parseInt(st.nextToken());
-			int y=Integer.parseInt(st.nextToken());
-			
-			//건물이 낮아지면 건물 추가, 높은 건물 pop
-			while(!stack.isEmpty() && stack.peek()>y) {
-				count++;
+		StringTokenizer st;
+		int result = 0;
+		for (int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			while(!stack.isEmpty() && stack.peek() > y){
+				result++;
 				stack.pop();
 			}
-			//같은 높이라면 추가할 필요 없음(길게 이어질 수 있기 때문에)
-			if(!stack.isEmpty() && stack.peek()==y)		continue;
-			//stack이 비어있거나 stack의 y값보다 큰 경우
+
+			if(!stack.isEmpty() && stack.peek() == y) continue;
 			stack.push(y);
 		}
-		
-		while(!stack.isEmpty()) {
-			//x는 1부터 시작하므로 0보다 클 때만 count 해야함
-			if(stack.peek() >0) count++;
+
+		while(!stack.isEmpty()){
+			if(stack.peek() !=0) result++;
 			stack.pop();
 		}
-			
-		System.out.println(count);
-	}
 
+		System.out.print(result);
+	}
 }
