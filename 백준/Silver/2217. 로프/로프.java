51b3c33a6592537ead908043183497fd85caf3fb
afteryoon <126151164+afteryoon@users.noreply.github.com>
2025-02-14 05:31:21 +0900

    [Silver IV] Title: 로프, Time: 292 ms, Memory: 26660 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,27 +1,28 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int n= Integer.parseInt(br.readLine());
-        int[] arr=new int[n];
 
-        for (int i = 0; i < n; i++)
-            arr[i] = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int min = 10000;
+		int max = 0;
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			min =Math.min(min,arr[i]);
+			max =Math.max(max,arr[i]);
+		}
 
-        Arrays.sort(arr);
+		Arrays.sort(arr);
 
-        int answer=0;
-        int temp=1;
-        for (int i = n-1; i >=0; i--) {
-            answer=Math.max(answer,arr[i]*temp);
-            temp++;
-        }
-        System.out.println(answer);
+		int k = 0;
+		for (int i=0; i<n; i++) {
+			k= Math.max(k, arr[i]*(n-i));
+		}
 
-    }
+		System.out.print(k);
+	}
 }
