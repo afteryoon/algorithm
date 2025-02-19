75c2dcb5531dce27a51d87c60e4c8b08c34c6060
afteryoon <126151164+afteryoon@users.noreply.github.com>
2025-02-19 09:05:36 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,39 +1,37 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int n,m;
-	static boolean[] isVisit;
 	static int[] arr;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args ) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		isVisit = new boolean[n+1];
+
 		arr = new int[m];
 
-		backTracking(0,1);
-		System.out.println(sb);
+		backtrack(0,1);
+
+		System.out.print(sb);
 	}
 
-	public static void backTracking(int depth, int start){
-		if(depth == m){
-			for (int val : arr){
-				sb.append(val).append(" ");
+	public static void backtrack(int depth, int start) {
+		if(depth == m) {
+			for (int i= 0; i<m; i++) {
+				sb.append(arr[i]).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 
-		for (int i = start; i <= n; i++) {
-
-				arr[depth] =i;
-				backTracking(depth+1,i);
+		for (int i=start; i<=n; i++) {
+			arr[depth] = i;
+			backtrack(depth+1,i);
 		}
 	}
 }
