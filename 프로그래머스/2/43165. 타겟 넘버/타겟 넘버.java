68403fe26e9d79aabebca7dcbbaa5e0d823d36fe
afteryoon <126151164+afteryoon@users.noreply.github.com>
2025-03-13 15:15:40 +0900

    [level 2] Title: 타겟 넘버, Time: 222.88 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,25 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers, int target) {
-    
-        return dfs(numbers,0,0,target);
-    }
-    
-    public static int dfs(int[] nums,int i,int sum, int target) {
-        int count = 0;
-        if(i == nums.length) {
-            if(target == sum) count++;
-            return count;
-        }
-        int n = nums[i];
-        count+= dfs(nums,i+1,sum+n,target);
-        count+= dfs(nums,i+1,sum-n,target);
+        int answer = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+      
         
-        return count;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int index = cur[0];
+            int sum = cur[1];
+
+            if(index == numbers.length) {
+                if(sum == target ) answer++;
+                continue;
+            }
+            int size = q.size();
+            q.add(new int[] {index+1,sum+numbers[index]});
+            q.add(new int[]{index+1,sum-numbers[index]});
+        }
+        
+        return answer;
     }
 }
