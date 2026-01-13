2349f4841fce6ae5257495187c210566fa5a08f1
afteryoon <126151164+afteryoon@users.noreply.github.com>
2026-01-13 17:14:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,28 +1,38 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int t=Integer.parseInt(br.readLine());
+	static int[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        int[] dp= new int[11];
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=4;
+		for (int i = 0; i < n; i++) {
+			int k = Integer.parseInt(br.readLine());
+			dp = new int[k + 1];
+			sb.append(dp(k)).append('\n');
+		}
 
-        for (int i = 4; i < dp.length; i++) {
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        }
+		System.out.println(sb);
+	}
 
-        StringBuilder sb= new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            sb.append(dp[Integer.parseInt(br.readLine())]+"\n");
-        }
-        System.out.println(sb);
-    }
+	static int dp(int k) {
+		if (k <= 3) {
+			if (k == 1) return 1;
+			if (k == 2) return 2;
+			return 4; // k == 3
+		}
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
 
+		for(int i=4; i<=k; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+
+		return dp[k];
+	}
 }
