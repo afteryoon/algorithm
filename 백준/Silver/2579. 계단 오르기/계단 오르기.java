b15fcc698094ba3b5e43ed79e55a7b82e345b4ef
afteryoon <126151164+afteryoon@users.noreply.github.com>
2026-01-14 01:27:34 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,28 +1,47 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 백준 2579 - 계단 오르기
+ * 규칙:
+ *  - 한 번에 1칸 또는 2칸 이동
+ *  - 연속된 3개의 계단을 모두 밟을 수 없음
+ *  - 마지막 계단은 반드시 밟아야 함
+ *
+ * dp[i] = i번째 계단(1-index)을 "반드시 밟는" 상태에서 얻을 수 있는 최대 점수
+ * 점화식(마지막 행동 분해):
+ *  1) i-2에서 2칸 점프해서 i를 밟는다: dp[i-2] + score[i]
+ *  2) i-3 -> i-1 -> i (연속 2칸 밟기) : dp[i-3] + score[i-1] + score[i]
+ * 따라서
+ *  dp[i] = max(dp[i-2], dp[i-3] + score[i-1]) + score[i]
+ */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] stairs = new int[n + 1];
-		int[] dp = new int[n + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-		for (int i = 1; i <= n; i++) {
-			stairs[i] = Integer.parseInt(br.readLine());
-		}
+        int[] score = new int[n + 1]; // 1-index
+        for (int i = 1; i <= n; i++) {
+            score[i] = Integer.parseInt(br.readLine());
+        }
 
-		// 기본 케이스 설정
-		if (n >= 1) dp[1] = stairs[1];
-		if (n >= 2) dp[2] = stairs[1] + stairs[2];
+        if (n == 1) {
+            System.out.println(score[1]);
+            return;
+        }
 
-		// DP
-		for (int i = 3; i <= n; i++) {
-			dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i]);
-		}
+        int[] dp = new int[n + 1];
+        dp[1] = score[1];
+        dp[2] = score[1] + score[2];
 
-		System.out.println(dp[n]);
-	}
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + score[i - 1]) + score[i];
+        }
+
+        System.out.println(dp[n]);
+    }
 }
