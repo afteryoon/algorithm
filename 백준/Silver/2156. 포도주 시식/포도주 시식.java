ee96c41895ee632247125dced7925b97b3c963f9
afteryoon <126151164+afteryoon@users.noreply.github.com>
2026-01-15 17:19:49 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,30 +1,49 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 백준 2156 - 포도주 시식
+ * 규칙: 연속 3잔을 마실 수 없음
+ *
+ * dp[i] = 1~i번째 잔까지 고려했을 때 마실 수 있는 최대 양
+ *
+ * 마지막 행동 분해(세 가지):
+ *  1) i번째 잔을 안 마신다               -> dp[i-1]
+ *  2) i번째 잔만 마신다( i-1은 안 마심 )  -> dp[i-2] + wine[i]
+ *  3) i-1, i를 연속으로 마신다( i-2는 안 마심 ) -> dp[i-3] + wine[i-1] + wine[i]
+ */
 public class Main {
-	//백준 2156
-	//
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n+1];
-		int[] dp = new int[n+1];
 
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-		dp[1] = arr[1];
-		if (n>=2)
-			dp[2] = arr[1]+arr[2];
+        int[] wine = new int[n + 1]; // 1-index
+        for (int i = 1; i <= n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
 
-		for (int i = 3; i <= n; i++) {
-			dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]+arr[i],dp[i-3]+arr[i]+arr[i-1]));
-		}
+        if (n == 1) {
+            System.out.println(wine[1]);
+            return;
+        }
 
-		System.out.println(dp[n]);
-	}
+        long[] dp = new long[n + 1];
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(
+                    dp[i - 1],
+                    Math.max(
+                            dp[i - 2] + wine[i],
+                            dp[i - 3] + wine[i - 1] + wine[i]
+                    )
+            );
+        }
+
+        System.out.println(dp[n]);
+    }
 }
