6ee32963c2dc2c346d562c0874f145162f456ba7
afteryoon <126151164+afteryoon@users.noreply.github.com>
2026-01-21 17:06:15 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 23988 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int left = 0;
+		int sum = 0;
+		int ans = Integer.MAX_VALUE;
+
+		for (int right = 0; right < n; right++) {
+			sum += arr[right];
+
+			while (sum >= s) {
+				ans = Math.min(ans, right - left + 1);
+				sum -= arr[left];
+				left++;
+			}
+		}
+
+		System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
+	}
+}
