2ba6204cc29ca49bc8ccb0a18d2aa01f932eb841
afteryoon <126151164+afteryoon@users.noreply.github.com>
2026-01-23 16:32:35 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2452 ms, Memory: 362216 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,89 +1,56 @@
 
 
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
 
-		for(int t = 0; t < T; t++) {
-			int k = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-			// 최대 힙과 최소 힙 선언
-			PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-			PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int t = Integer.parseInt(br.readLine());
 
-			// 각 숫자의 개수를 저장하는 맵
-			Map<Integer, Integer> countMap = new HashMap<>();
+        for (int tc = 0; tc < t; tc++) {
+            int k = Integer.parseInt(br.readLine());
 
-			for(int i = 0; i < k; i++) {
-				String[] op = br.readLine().split(" ");
-				char command = op[0].charAt(0);
-				int n = Integer.parseInt(op[1]);
+            TreeMap<Integer, Integer> map = new TreeMap<>();
 
-				if(command == 'I') {
-					// 삽입 연산
-					minHeap.offer(n);
-					maxHeap.offer(n);
-					countMap.put(n, countMap.getOrDefault(n, 0) + 1);
-				} else if(command == 'D') {
-					if(countMap.isEmpty()) continue; // 큐가 비어있으면 무시
+            for (int i = 0; i < k; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String cmd = st.nextToken();
+                int x = Integer.parseInt(st.nextToken());
 
-					if(n == 1) {
-						// 최댓값 삭제
-						removeFromHeap(maxHeap, countMap);
-					} else if(n == -1) {
-						// 최솟값 삭제
-						removeFromHeap(minHeap, countMap);
-					}
-				}
+                if (cmd.equals("I")) {
+                    map.put(x, map.getOrDefault(x, 0) + 1);
+                } else { // "D"
+                    if (map.isEmpty()) continue;
 
-				// 동기화 - 이미 삭제된 값들을 힙에서 제거
-				while(!minHeap.isEmpty() && countMap.getOrDefault(minHeap.peek(), 0) == 0) {
-					minHeap.poll();
-				}
-				while(!maxHeap.isEmpty() && countMap.getOrDefault(maxHeap.peek(), 0) == 0) {
-					maxHeap.poll();
-				}
-			}
+                    if (x == 1) {
+                        // delete max
+                        int key = map.lastKey();
+                        int cnt = map.get(key);
+                        if (cnt == 1) map.remove(key);
+                        else map.put(key, cnt - 1);
+                    } else {
+                        // delete min (x == -1)
+                        int key = map.firstKey();
+                        int cnt = map.get(key);
+                        if (cnt == 1) map.remove(key);
+                        else map.put(key, cnt - 1);
+                    }
+                }
+            }
 
-			// 결과 출력
-			if(countMap.isEmpty()) {
-				sb.append("EMPTY\n");
-			} else {
-				// 마지막 동기화
-				while(!minHeap.isEmpty() && countMap.getOrDefault(minHeap.peek(), 0) == 0) {
-					minHeap.poll();
-				}
-				while(!maxHeap.isEmpty() && countMap.getOrDefault(maxHeap.peek(), 0) == 0) {
-					maxHeap.poll();
-				}
+            if (map.isEmpty()) {
+                sb.append("EMPTY\n");
+            } else {
+                sb.append(map.lastKey()).append(' ').append(map.firstKey()).append('\n');
+            }
+        }
 
-				sb.append(maxHeap.peek() + " " + minHeap.peek() + "\n");
-			}
-		}
-
-		System.out.print(sb);
-	}
-
-	// 힙에서 요소를 제거하고 맵에서 카운트 감소
-	private static void removeFromHeap(PriorityQueue<Integer> heap, Map<Integer, Integer> countMap) {
-		while(!heap.isEmpty()) {
-			int value = heap.poll();
-			int count = countMap.getOrDefault(value, 0);
-
-			if(count > 0) {
-				// 개수 감소 또는 맵에서 제거
-				if(count == 1) {
-					countMap.remove(value);
-				} else {
-					countMap.put(value, count - 1);
-				}
-				break;
-			}
-		}
-	}
+        System.out.print(sb);
+    }
 }
