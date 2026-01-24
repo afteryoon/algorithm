d9834ab1a806cb61db363e52663a38352afeafa6
afteryoon <126151164+afteryoon@users.noreply.github.com>
2026-01-25 00:04:38 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 21948 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,39 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n,m;
+	static int N, M;
 	static int[] result;
 	static boolean[] isUse;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args ) throws Exception {
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
-		result = new int[n+1];
-		isUse = new boolean[n+1];
-
-		backtrack(0);
-
-		System.out.print(sb);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		sb = new StringBuilder();
+		result = new int[M];
+		isUse = new boolean[N+1];
+		bk(0);
+		System.out.println(sb);
 	}
 
-	public static void backtrack(int depth) {
-		if(depth == m) {
-			for(int i = 0; i<m; i++) {
-				sb.append(result[i]).append(" ");
-			}
-			sb.append("\n");
+	static void bk(int depth) {
+		if(depth == M) {
+			for(int i = 0; i < M; i++) sb.append(result[i]).append(" ");
+			sb.append('\n');
 			return;
 		}
-		for(int i = 1; i<=n; i++) {
-			if(!isUse[i]) {
+		for(int i = 1; i <= N; i++) {
+			if(!isUse[i]){
 				isUse[i] = true;
 				result[depth] = i;
-				backtrack(depth+1);
+				bk(depth + 1);
 				isUse[i] = false;
 			}
 		}
-
 	}
 }
